98710255479a3e8d30f8978fb1ec6370e2473383
Sleve-m <jacekellyofficial@gmail.com>
2026-01-02 01:07:40 -0500

    v1.0.1b

--- modules/moreInstance.lua
+++ modules/moreInstance.lua
@@ -6,12 +6,19 @@ local function modify(inst, props)
     end
 end
 
+local function destroychildren(inst)
+    for i, v in pairs(inst:GetChildren()) do
+        v:Destroy()
+    end
+end
+
 function moreInstance.load()
     local RealInstance = Instance
     local FakeInstance = {}
     FakeInstance.new = function(...) return RealInstance.new(...) end
     FakeInstance.fromExisting = function(...) return RealInstance.fromExisting(...) end
     FakeInstance.modify = modify
+    FakeInstance.DestroyChildren = destroychildren
     gettenv(coroutine.running()).Instance = FakeInstance
     setreadonly(gettenv(coroutine.running()).Instance, true)
 end
